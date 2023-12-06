eebe1fc60c3f04537cc7554423d020413c1ea2dd
Shrinibas Mahanta <shrinibasmahanta2004@gmail.com>
2023-12-07 01:59:02 +0530

    adds ble server.

--- go.mod
+++ go.mod
@@ -2,15 +2,14 @@ module github.com/2k4sm/rpcomph-poc
 
 go 1.21.4
 
-require tinygo.org/x/bluetooth v0.8.0
+require github.com/go-ble/ble v0.0.0-20230130210458-dd4b07d15402
 
 require (
-	github.com/fatih/structs v1.1.0 // indirect
-	github.com/go-ole/go-ole v1.2.6 // indirect
-	github.com/godbus/dbus/v5 v5.1.0 // indirect
-	github.com/muka/go-bluetooth v0.0.0-20221213043340-85dc80edc4e1 // indirect
-	github.com/saltosystems/winrt-go v0.0.0-20230921082907-2ab5b7d431e1 // indirect
-	github.com/sirupsen/logrus v1.9.3 // indirect
-	github.com/tinygo-org/cbgo v0.0.4 // indirect
+	github.com/mattn/go-colorable v0.1.6 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/mgutz/ansi v0.0.0-20170206155736-9520e82c474b // indirect
+	github.com/mgutz/logxi v0.0.0-20161027140823-aebf8a7d67ab // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/stretchr/testify v1.7.5 // indirect
 	golang.org/x/sys v0.11.0 // indirect
 )
--- server.go
+++ server.go
@@ -1,25 +1,44 @@
 package main
 
 import (
-	"tinygo.org/x/bluetooth"
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/go-ble/ble"
+	"github.com/go-ble/ble/linux"
 )
 
-var adapter = bluetooth.DefaultAdapter
-
-func main() {
-	// Enable BLE interface.
-	must("enable BLE stack", adapter.Enable())
-
-	// Start scanning.
-	println("scanning...")
-	err := adapter.Scan(func(adapter *bluetooth.Adapter, device bluetooth.ScanResult) {
-		println("found device:", device.Address.String(), device.RSSI, device.LocalName())
-	})
-	must("start scan", err)
+func must(desc string, err error) {
+	if err != nil {
+		fmt.Printf("%s: %s\n", desc, err)
+	}
 }
 
-func must(action string, err error) {
-	if err != nil {
-		panic("failed to " + action + ": " + err.Error())
-	}
+func main() {
+	// Initialize the Bluetooth adapter.
+	d, err := linux.NewDevice()
+	must("new device", err)
+	//defer d.Stop()
+
+	// Set a timeout for scanning (e.g., 10 seconds).
+	timeout := 100 * time.Second
+
+	// Start scanning for BLE devices.
+	fmt.Println("Scanning for devices...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	// Discover devices during the scan.
+	err = d.Scan(ctx, false, func(a ble.Advertisement) {
+
+		if a.LocalName() == "Poco M6 Pro" {
+			fmt.Printf("Device found: %s\n", a.Addr())
+		}
+	})
+
+	must("scan", err)
+
+	fmt.Println("Scan completed.")
 }
